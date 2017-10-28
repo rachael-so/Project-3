935bc445deecfb08f3a51ac70691363f56833843
rachael-so <rachaelso@gmail.com>
2017-10-28 16:30:13 -0400

    create project

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  p3
+//
+//  Created by Rachael So on 10/28/17.
+//  Copyright © 2017 Rachael So. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
